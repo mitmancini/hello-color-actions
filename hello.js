d7ee81f7123248fca60a87218ebf707a3f727dc7
mitmancini <stefania.mancini.bosanac@gmail.com>
2024-05-19 15:27:55 -0400

    Change to include Name and Location

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Stefania Mancini In Canada!";
 console.log(hello());
 exports.hello = hello;
 
